728352f6d689f6c7cf5454d8c71b7f3ad7611e94
Anant1202 <anantsingh1202@gmail.com>
2022-05-10 18:29:02 +0530

    Added:Perfect Number Program

---
+++ LogicalProblems/PerfectNumber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProblems
+{
+    public class PerfectNumber
+    {
+        public void CalculatingSum()
+        {
+            int sum = 0;
+            Console.WriteLine("Enter a number to check whether it is Perfect Number or not");
+            int num=Convert.ToInt32(Console.ReadLine());
+            for(int i=1;i<=num/2;i++)
+            {
+                if(num%i==0)
+                {
+                    sum += i;
+                    Console.Write(i+" " );
+                }
+            }
+            Console.WriteLine(" Sum= "+sum);
+        }
+    }
+}
--- LogicalProblems/Program.cs
+++ LogicalProblems/Program.cs
@@ -7,8 +7,10 @@ namespace LogicalProblems
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Logical Problem Programs");
-            FibonacciSeries ob1= new FibonacciSeries();
-            ob1.CalculatingSeries();
+            //FibonacciSeries ob1= new FibonacciSeries();
+            //ob1.CalculatingSeries();
+            PerfectNumber ob2= new PerfectNumber();
+            ob2.CalculatingSum();
         }
     }
 }
