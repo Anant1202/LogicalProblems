63bf76ae9b6a89c066ba776245c7334e733ff883
Anant1202 <anantsingh1202@gmail.com>
2022-05-10 16:39:39 +0530

    Welcome to Logical Problems Program

---
+++ LogicalProblems/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LogicalProblems
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Logical Problem Programs");
+        }
+    }
+}
+
