4c37d4a721a5be2ee74671f699b86cf2cdc3fa54
Anant1202 <anantsingh1202@gmail.com>
2022-05-12 17:33:30 +0530

    Added:Stopwatch Program

--- LogicalProblems/Program.cs
+++ LogicalProblems/Program.cs
@@ -7,15 +7,37 @@ namespace LogicalProblems
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Logical Problem Programs");
-            //FibonacciSeries ob1= new FibonacciSeries();
-            //ob1.CalculatingSeries();
-            //PerfectNumber ob2= new PerfectNumber();
-            //ob2.CalculatingSum();
-            //PrimeNumber ob4= new PrimeNumber();
-            //ob4.CheckingPrimeOrNot();
-            ReverseNumber ob3=new ReverseNumber();
-            ob3.Reverse();
+            Console.WriteLine("Enter an option");
+            int option = Convert.ToInt32(Console.ReadLine());
+            while (option<6)
+            {
+                switch (option)
+                {
+                    case 1:
+                        FibonacciSeries ob1 = new FibonacciSeries();
+                        ob1.CalculatingSeries();
+                        return;
+                    case 2:
+                        PerfectNumber ob2 = new PerfectNumber();
+                        ob2.CalculatingSum();
+                        return;
+                    case 3:
+                        PrimeNumber ob4 = new PrimeNumber();
+                        ob4.CheckingPrimeOrNot();
+                        return;
+                    case 4:
+                        ReverseNumber ob3 = new ReverseNumber();
+                        ob3.Reverse();
+                        return;
+                    case 5:
+                        Stopwatch ob5 = new Stopwatch();
+                        ob5.StopwatchProgram();
+                        return;
+                    default:
+                        Console.WriteLine("Enter value between 1 to 6");
+                        return;
+                }
+            }
         }
     }
 }
-
---
+++ LogicalProblems/Stopwatch.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProblems
+{
+    public class Stopwatch
+    {
+        public void StopwatchProgram()
+        {
+            Console.Write("Start the Stopwatch.Enter S or s:");
+            char ch=Convert.ToChar(Console.Read());
+            //Stopwatch sw = Stopwatch.StartNew();
+            if(ch=='s' || ch=='S')
+            {
+                //sw.Start();
+                Console.Write("\nStop the Stopwatch.Enter E or e:");
+                char end=Convert.ToChar(Console.Read());
+                if (end == 'e' || end == 'E')
+                {
+                    //sw.Stop();
+                    //Console.WriteLine("\n Time Elapsed:" + sw.Elapsed.TotalSeconds + "seconds");
+                }
+                else
+                {
+                    Console.WriteLine("Wrong Input");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Wrong Input");
+            }
+        }
+    }        
+}
